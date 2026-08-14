433231c9b35cc48370eb856faca21d38f79da91f
Bhagyashree Biradar <bhagyabiradar63@gmail.com>
2026-08-14 22:39:53 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,12 @@
+# Creating a tuple
+numbers = (10, 20, 30, 40)
+
+# Accessing elements
+print("First number:", numbers[0])
+
+# Length of tuple
+print("Length:", len(numbers))
+
+# Iterating through tuple
+for num in numbers:
+    print(num)
