1daa68726f7553f810e62ad3975dc1cc4b1f7b80
Bhagyashree Biradar <bhagyabiradar63@gmail.com>
2026-08-14 22:40:14 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+# Creating a list
+fruits = ["Apple", "Banana", "Mango"]
+
+# Accessing elements
+print("First fruit:", fruits[0])
+
+# Adding an element
+fruits.append("Orange")
+print("After adding:", fruits)
+
+# Modifying an element
+fruits[1] = "Grapes"
+print("After modifying:", fruits)
+
+# Removing an element
+fruits.remove("Apple")
+print("After removing:", fruits)
